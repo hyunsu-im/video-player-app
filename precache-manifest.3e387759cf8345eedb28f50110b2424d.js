7feb506e7166fe81e10f2b83cc5e0b9c494ec7f0
Hyunsu Im <dev.hyunsu@gmail.com>
2018-10-09 18:09:57 +0900

    Updates

---
+++ precache-manifest.3e387759cf8345eedb28f50110b2424d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/video-player-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c3ef3530f26cd4699677",
+    "url": "/video-player-app/static/js/runtime~main.c3ef3530.js"
+  },
+  {
+    "revision": "d6252221026933725ca7",
+    "url": "/video-player-app/static/js/main.d6252221.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/video-player-app/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "d6252221026933725ca7",
+    "url": "/video-player-app/static/css/main.a615ea67.chunk.css"
+  },
+  {
+    "revision": "a8c4b2a007ccc5b674ed0fbce967e873",
+    "url": "/video-player-app/index.html"
+  }
+];
